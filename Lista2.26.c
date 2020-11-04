9fe24b6e0f298a497d586cdf70a84071e9fb056b
Marcelo Martins Ferreira <70519529+MrSoad@users.noreply.github.com>
2020-11-03 22:26:25 -0300

    Update Lista2.26.c

--- Lista2.26.c
+++ Lista2.26.c
@@ -2,11 +2,18 @@
 #include<stdlib.h>
 #include "bbmt.h"
 int main(){
-	int mt[100][100],x;
+	int mt[100][100],x,i,j;
 	printf("Informe n: ");
 	scanf("%d",&x);
 
-	geraMat(mt,x,x);
+	for(i=0;i<x;i++){
+		for(j=0;j<x;j++){
+			if(i == j){
+				mt[i][j]=1;
+			}else
+			mt[i][j]=0;
+		}
+	}
 	mostraMat(mt,x,x);
 
 	system("pause");
